8700ab3325de006c10f3f96fa764dd2003e929c3
echchafykyotmane <otmaneechchafyky5@gmail.com>
2023-04-24 16:19:37 +0100

    All books saved in the collection displayed in the top part of the page

--- script.js
+++ script.js
@@ -24,3 +24,16 @@ function removeBook (bookTitle) {
     const result = books.filter(book => book.title != bookTitle);
     return result;
 };
+
+const article = [];
+const bookList = document.querySelector('.bookList');
+
+for( let i = 0; i < books.length; i += 1) {
+    article[i] = document.createElement('article');
+    bookList.appendChild(article[i]);
+    article[i].innerHTML = `
+        <span>"${books[i].title}"</span>by 
+        <span>${books[i].author}</span>
+        <button type="button" id="${books[i].bookId}">Remove</button>
+    `;
+}
